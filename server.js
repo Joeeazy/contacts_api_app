689579f28c562dd75948deffe36892fa4e3ef544
Joeeazy <joeeazy34@gmail.com>
2024-10-18 14:20:14 +0300

    routes created

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import contactRoutes from "./routes/contactRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/contacts", contactRoutes);
+
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`);
+});
